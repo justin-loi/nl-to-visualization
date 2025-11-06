853b4aa3b06706a78d210452ca446c8cd11aa422
justin-loi <jmloi@hawaii.edu>
2025-11-05 17:38:20 -1000

    clean up and comment for future use

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,3 +1,4 @@
+// Will keep to measure performance metrics for future version
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
